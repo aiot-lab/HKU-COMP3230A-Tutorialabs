02d7212d43d22333aeb6c29f8d7cbc4276fe1e48
HOU Weiying <wyhou@cs.hku.hk>
2022-09-13 11:21:33 +0800

    update several lines

--- C-basic/01-gcc-basic.c
+++ C-basic/01-gcc-basic.c
@@ -1,5 +1,7 @@
 // file: 01-gcc-basic.c
 // description: try basic gcc command
+
+// preprocessor, include stdio.h
 #include <stdio.h>
 
 // GCC is a compiler, it will compile the source code into machine code 
@@ -7,6 +9,13 @@
 // GCC basic usage: gcc -o <output> <input> <options>
 // To compile: `gcc -o 01-gcc-basic 01-gcc-basic.c` 
 // To run the executable file: `./01-gcc-basic`
+
+// othe GCC options:
+// -S: compile to assembly code
+// -c: compile to object file
+// -E: preprocess only
+// -g: generate debug information
+// -O: optimize the code
 int main(int argc, char *argv[]) {
     printf("Congratulations! You have successfully compiled and run this program.");
     return 0;
