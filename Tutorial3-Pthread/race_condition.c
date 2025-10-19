677e8fe054b3bccd6fcd3c98474832dd4f3d949d
Kong-To <62915360+1ucayu@users.noreply.github.com>
2025-10-19 17:11:38 +0800

    add an example of mutex

--- Tutorial3-Pthread/race_condition.c
+++ Tutorial3-Pthread/race_condition.c
@@ -47,19 +47,6 @@ int main() {
 // int a = 0, iret;
 // pthread_mutex_t lock;
 
-// // void* thr_func(void* arg) {
-// //     for (int i = 0; i < 1000000; i++) {
-// //         if (pthread_mutex_trylock(&lock_a) == 0) {
-// //             a++;
-// //             pthread_mutex_unlock(&lock_a);
-// //         } else if (pthread_mutex_trylock(&lock_b) == 0) {
-// //             b++;
-// //             pthread_mutex_unlock(&lock_b);
-// //         }
-// //     }
-// //     pthread_exit(NULL);
-// // }
-
 // void* thr_func(void* arg) {
 //     for (int i = 0; i < 1000000; i++) {
 //         pthread_mutex_lock(&lock);
@@ -97,3 +84,48 @@ int main() {
 // }
 
 
+// Mutex lock - multiple threads
+
+// int a = 0, b = 0, iret;
+// pthread_mutex_t lock_a, lock_b;
+
+// void* thr_func(void* arg) {
+//     for (int i = 0; i < 1000000; i++) {
+//         if (pthread_mutex_trylock(&lock_a) == 0) {
+//             a++;
+//             pthread_mutex_unlock(&lock_a);
+//         } else if (pthread_mutex_trylock(&lock_b) == 0) {
+//             b++;
+//             pthread_mutex_unlock(&lock_b);
+//         }
+//     }
+//     pthread_exit(NULL);
+// }
+
+// int main() {
+//     pthread_t thr1, thr2;
+//     pthread_mutex_init(&lock_a, NULL);
+//     pthread_mutex_init(&lock_b, NULL);
+//     iret = pthread_create(&thr1, NULL, thr_func, NULL);
+//     if (iret != 0) { 
+//         perror("Cannot Create Thread"); 
+//     }
+//     iret = pthread_create(&thr2, NULL, thr_func, NULL);
+//     if (iret != 0) { 
+//         perror("Cannot Create Thread"); 
+//     }
+
+//     iret = pthread_join(thr1, NULL);
+//     if (iret != 0) { 
+//         perror("Cannot Collect Thread"); 
+//     }
+//     iret = pthread_join(thr2, NULL);
+//     if (iret != 0) { 
+//         perror("Cannot Collect Thread"); 
+//     }
+
+//     pthread_mutex_destroy(&lock_a);
+//     pthread_mutex_destroy(&lock_b);
+//     printf("a = %d, b = %d\n", a, b);
+//     printf("a + b = %d\n", a + b);
+// }
